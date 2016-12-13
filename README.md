f8b4ffb95158c227678607b2c7591af71fb6147e
Willian Krueger <wkrueger128@gmail.com>
2016-12-13 14:59:08 -0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # buildscript-utils
-I am a hipster and I don't need gulp!
+
+> _I am a hipster and I don't need gulp!_
+
+**Stub module status:** This is a "stub" module. It's someting taken out from a another
+project, it will mostly work fine since its "battle-tested", but it's not polished
+enough to deserve an npm module.
+
+## Why not gulp
+
+  - Easier debugging  
+  - Flat dependencies. (No gulp plugins which require the real APIs)
+  - The use of gulp tasks as async dependencies is BAD (admitelly said by their creators)
+  - Because it is not needed
+  - Because I'm a hipster and I like to invent wheels
+
+
+## Tools in a nutshell
+
+### runTask(tasksDef : { [taskName] : () => Promise|void })
+
+The task runner. Provide and object which keys will be the task names.
+
+```javascript
+//build.js
+var tasks = {
+    bundle() {
+        return doSomething()
+    } 
+}
+buildscript.runTask(tasks)
+//call in console:
+//node build bundle
+```
+Dependencies? Do them yourself using promises and raw functions.  
+Parameters? Do them yourself in your favorite way. (todo: add default CLI param passing)
+
+### _p or promisify( fn, ...args ) => Promise<any[]>
+
+Convert a generic callback-style function to a Promise.  
+The last argument is assumed as the node-style callback `(err, result) => {}`.
+
+You may need to rebind the function/method if the source API has OOP-style.
+
+```javascript
+const request = require('request')
+bs._p( request , { method : 'GET', url: 'url' } )
+    .then( [response, body] => {
+        console.log(body)
+    })
+```
+
+### spawn( command, cwd?, processCb? : (process) => any ) : Promise<void>
+
+Spawns a CLI tool.  
+TODO: Add option to automatically prepend '../node_modules/.bin'.
+
+**processCb** Callback which provides the created process object.
+You are not supposed to use this unless you want to -- say -- programatically
+send input or kill the process.
+
+```javascript
+var tasks = {
+    watch() {
+        bs.spawn('../node_modules/.bin/tsc -w')
+        bs.spawn('../node_modules/.bin/livereload')
+    }
+}
+bs.runTask(tasks)
+```
+
+### copyFn( srcglob, dest ) : Promise<void>
+
+Copies a glob of files to a destination, flattening the output tree.
+To copy a folder without flattening, use `spawn('fsync')` instead.
+
+
+---
+
+_(The other functions inside here either dont belong here, or are not being used:
+therefore are badly tested.)_
---
+++ src/index.js
@@ -0,0 +1,182 @@
+const through = require('through2')
+const sudo = require('sudo')
+const cp = require('child_process')
+const fs = require('fs')
+const yargs = require('yargs')
+const path = require('path')
+const glob = require('glob')
+
+/**
+ * Promisify: converts node-style callback to a promise
+ */
+function _p( fn, ...args ) {
+    return new Promise( (resolve, reject) => {
+        fn( ...args , (err, ...result) => {
+            if (err) return reject(err)
+            resolve(result)
+        })
+    })
+}
+exports.promisify = _p
+exports._p = _p
+
+
+
+/**
+ * Bind.
+ */
+exports.bind = function bind( obj, fn ) {
+    return (...args) => {
+        obj[fn].apply(obj, args)
+    }
+}
+
+
+function prepend(text) {
+    return through(function(chunk, enc, cb){
+        let str = chunk.toString()
+        str = text + str
+        this.push(new Buffer(str))
+        cb()
+    })
+}
+
+
+/**
+ * Spawns process, pipes to stdout and stderr, returns promise which
+ * resolves when process finishes 
+ */
+exports.spawn = function spawn(cmd, cwd, processCb) {
+    console.log('spawn'.yellow, cmd)
+    var isSudo = false
+    var split = cmd.split(' ')
+    if (split[0] == 'sudo') {
+        isSudo = true
+        split.unshift
+    }
+    var path = split[0]
+    if (path.charAt(0) === '.') path = require('path').resolve(path)
+    split.shift()
+    return new Promise( (resolve,reject) => {
+        var opts = undefined
+        if (cwd) {
+            cwd = require('path').resolve(cwd)
+            opts = { cwd }
+        }
+        let ps;
+        if (!isSudo) ps = cp.spawn(path, split, opts)
+        else {
+            let args = [ path, ...split ]
+            let sudoopts
+            if ( cwd ) sudoopts = { spawnOptions : opts, cachePassword:false }
+            ps = sudo(args, sudoopts)
+        }
+        let out = ps.stdout
+        let err = ps.stderr
+        if (!yargs.argv.o) {
+            out = out.pipe(prepend('> '))
+            err = err.pipe(prepend('err> '))
+        }
+        out.pipe(process.stdout)
+        err.pipe(process.stderr)
+        ps.on('close', () => {
+            console.log('end'.yellow, cmd)
+            resolve()
+        })
+        ps.on('error', () => {
+            console.log('error'.yellow, cmd)
+            reject()
+        })
+        processCb && processCb(ps)
+    })
+}
+
+
+/**
+ * Copy a glob of files to a destination. Flattens the output tree.
+ * To copy a whole folder whith its structure, use spawn fsync.
+ */
+exports.copyFn = function copyFn( _srcglob, _dest ) {
+    if (!Array.isArray(_srcglob)) _srcglob = [_srcglob]
+    return _srcglob.reduce( (chain, eachglob) => {
+        return chain.then(() => {
+            return exports._p( glob, eachglob ).then( files => {
+                let all = files.map( f => {
+                    let base = path.basename(f)
+                    let src = path.resolve(f)
+                    let dest =  _dest + '/' + base
+                    return new Promise( res => {
+                        fs.createReadStream(src)
+                            .pipe(fs.createWriteStream(dest))
+                            .on('finish', () => {
+                                console.log('copyfn '.yellow + f + ' => '.red + dest)
+                                res()
+                            })
+                    })
+                })
+                return Promise.all(all)
+            })
+        })
+    }, Promise.resolve() )      
+}
+
+
+var debouncer = {}
+exports.debounce = function(id, delay, fn) {
+    //console.log('deb1')
+    return function(...args) {
+        //console.log('deb2',id)
+        var last = debouncer[id] || 0
+        var now = new Date().getTime()
+        if (now - last > delay) {
+            debouncer[id] = now + delay
+            setTimeout(() => fn(...args) ,delay)
+        }
+    }
+}
+
+
+exports.requestP = function(reqparams) {
+    reqparams.json = reqparams.json || true
+    console.log(reqparams.method, reqparams.url)
+    var request = require('request')
+    return new Promise( (res,rej) => {
+        request( reqparams, (err,response,body) => {
+            console.log('request responded')
+            if (err) return rej(err)
+            if (String(response.statusCode).charAt(0) !== '2') {
+                console.error('response code', response.statusCode)
+                if (Array.isArray(body)) {
+                    let message = body.reduce((bef,curr)=>bef + curr.field + ': ' + curr.message +';','')
+                    return rej({ message })
+                }
+                else rej(body)
+            }            
+            if (reqparams.method === 'POST' && typeof body !== 'object') 
+                return rej(String(body));
+            res(body||{})
+        })            
+    }) 
+}
+
+// -- the Runner -- //
+
+exports.runTask = function(tasksDef = {}) {
+    let taskName = yargs.argv._[0]
+    let fn = tasksDef[taskName]
+    if (!fn) return console.error('Task ' + taskName + ' not found')
+    var run = fn()
+
+    //if the task is a promise, it will end sometime
+    if (run && run.then) {
+        run.then( () => {
+            console.log('Finished')
+            process.exit(0)
+        })
+        .catch( err => {
+            console.error('Error on task'.red)
+            console.error(err && (err.formatted || err))
+            process.exit(0)
+        })
+    }
+}
